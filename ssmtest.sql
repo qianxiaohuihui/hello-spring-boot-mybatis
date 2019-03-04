a4719941db808e5efc887db2313d19167f925f6d
Ann <15732151912@163.com>
2019-03-04 17:59:32 +0800

    SQL

---
+++ ssmtest.sql
@@ -0,0 +1,62 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 5.1.55-community : Database - ssmtest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssmtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssmtest`;
+
+/*Table structure for table `appuser` */
+
+DROP TABLE IF EXISTS `appuser`;
+
+CREATE TABLE `appuser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_name` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `appuser` */
+
+insert  into `appuser`(`id`,`login_name`,`user_name`,`password`,`age`) values 
+(1,'user','user','123456',1),
+(2,'admin','管理员','123',1),
+(3,'user1','user1','123',1),
+(4,'user2','user2','123',1);
+
+/*Table structure for table `paper` */
+
+DROP TABLE IF EXISTS `paper`;
+
+CREATE TABLE `paper` (
+  `paper_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'paperID',
+  `name` varchar(100) NOT NULL COMMENT 'paper名称',
+  `number` int(11) NOT NULL COMMENT 'paper数量',
+  `detail` varchar(200) NOT NULL COMMENT 'paper描述',
+  PRIMARY KEY (`paper_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='paper表';
+
+/*Data for the table `paper` */
+
+insert  into `paper`(`paper_id`,`name`,`number`,`detail`) values 
+(1,'测试1',1,'测试详情'),
+(2,'深度学习',3,'dldldl'),
+(3,'大数据',4,'bdbdbd');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
